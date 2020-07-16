44bdea7418f5fdc8a896ad1608fcdc9a52e0318f
greenhornsr <srollins11279@gmail.com>
2020-07-16 12:57:32 -0700

    added local state, change handler function for select form

--- event-tracker/src/components/form/EventList.jsx
+++ event-tracker/src/components/form/EventList.jsx
@@ -2,13 +2,20 @@ import React, {useState} from 'react'
 import "./form.css"
 
 const EventList = () => {
-    const [currState, setCurrState] = useState(false)
+    const [currState, setCurrState] = useState({value : ''})
 
     // const setStateHelper = () => {
         
     // }
 
-    {console.log(currState)}
+    // change handler
+    const handleChange = (event) => {
+        event.preventDefault()
+        {console.log("TEST: ", event)}
+        setCurrState({value : event.target.value})
+        console.log("currState: ", event.target.value)
+    }
+    
     return (
         <div>
             <p>DAMN TEST!</p>
@@ -19,9 +26,9 @@ const EventList = () => {
 
                 <form action="">
                     <label htmlFor="occurrance">Did you have an EVENT? {"\u00a0 \u00a0"} </label>
-                    <select onChange={setCurrState(event.target.value)} required name="occurrance" id="occurrance" value={currState} >
+                    <select required onChange={handleChange} name="occurrance" id="occurrance" value={currState.value} >
                         <option value="select" defaultValue >Please Select...</option>
-                        <option on value="true">YES</option>
+                        <option value="true">YES</option>
                         <option value="false">NO</option>
                     </select><br/>
                     <button>Submit</button>
